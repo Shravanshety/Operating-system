5030d0de2a5c374c018f361d78349119e706b7e8
Shravanshety <shravanshetty.cs24@bmsce.ac.in>
2025-05-18 11:37:44 +0530

    Update Roundrobin.c

--- week3/Roundrobin.c
+++ week3/Roundrobin.c
@@ -2,91 +2,62 @@
 
 #define MAX 100
 
-void priorityPreemptive(int n, int at[], int bt[], int pr[]) {
-    int ct[n], tat[n], wt[n], rem_bt[n], is_completed[n];
-    int time = 0, completed = 0, min_priority, index;
+void roundRobin(int n, int at[], int bt[], int quant) {
+    int ct[n], tat[n], wt[n], rem_bt[n];
+    int queue[MAX], front = 0, rear = 0;
+    int time = 0, completed = 0, visited[n];
 
     for (int i = 0; i < n; i++) {
         rem_bt[i] = bt[i];
-        is_completed[i] = 0;
+        visited[i] = 0;
     }
+
+    queue[rear++] = 0;
+    visited[0] = 1;
 
     while (completed < n) {
-        min_priority = 9999;
-        index = -1;
-
-        for (int i = 0; i < n; i++) {
-            if (at[i] <= time && is_completed[i] == 0 && pr[i] < min_priority && rem_bt[i] > 0) {
-                min_priority = pr[i];
-                index = i;
-            }
-        }
-
-        if (index == -1) {
-            time++;
+        int index = queue[front++];
+       
+        if (rem_bt[index] > quant) {
+            time += quant;
+            rem_bt[index] -= quant;
         } else {
-            rem_bt[index]--;
-            time++;
-
-            if (rem_bt[index] == 0) {
-                ct[index] = time;
-                is_completed[index] = 1;
-                completed++;
-            }
-        }
-    }
-
-    float total_tat = 0, total_wt = 0;
-    printf("\nP#\tAT\tBT\tPR\tCT\tTAT\tWT\n");
-    for (int i = 0; i < n; i++) {
-        tat[i] = ct[i] - at[i];
-        wt[i] = tat[i] - bt[i];
-        total_tat += tat[i];
-        total_wt += wt[i];
-        printf("%d\t%d\t%d\t%d\t%d\t%d\t%d\n", i + 1, at[i], bt[i], pr[i], ct[i], tat[i], wt[i]);
-    }
-
-    printf("Average TAT: %.2f\n", total_tat / n);
-    printf("Average WT: %.2f\n", total_wt / n);
-}
-
-void priorityNonPreemptive(int n, int at[], int bt[], int pr[]) {
-    int ct[n], tat[n], wt[n], is_completed[n], rem_bt[n];
-    int time = 0, completed = 0;
-
-    for (int i = 0; i < n; i++) {
-        is_completed[i] = 0;
-        rem_bt[i] = bt[i];
-    }
-
-    while (completed < n) {
-        int min_priority = 9999, index = -1;
-
-        for (int i = 0; i < n; i++) {
-            if (at[i] <= time && is_completed[i] == 0 && pr[i] < min_priority) {
-                min_priority = pr[i];
-                index = i;
-            }
-        }
-
-        if (index == -1) {
-            time++;
-        } else {
-            time += bt[index];
+            time += rem_bt[index];
+            rem_bt[index] = 0;
             ct[index] = time;
-            is_completed[index] = 1;
             completed++;
         }
+
+        for (int i = 0; i < n; i++) {
+            if (at[i] <= time && rem_bt[i] > 0 && !visited[i]) {
+                queue[rear++] = i;
+                visited[i] = 1;
+            }
+        }
+
+        if (rem_bt[index] > 0) {
+            queue[rear++] = index;
+        }
+
+        if (front == rear) {
+            for (int i = 0; i < n; i++) {
+                if (rem_bt[i] > 0) {
+                    queue[rear++] = i;
+                    visited[i] = 1;
+                    break;
+                }
+            }
+        }
     }
 
     float total_tat = 0, total_wt = 0;
-    printf("\nP#\tAT\tBT\tPR\tCT\tTAT\tWT\n");
+    printf("P#\tAT\tBT\tCT\tTAT\tWT\n");
     for (int i = 0; i < n; i++) {
         tat[i] = ct[i] - at[i];
         wt[i] = tat[i] - bt[i];
         total_tat += tat[i];
         total_wt += wt[i];
-        printf("%d\t%d\t%d\t%d\t%d\t%d\t%d\n", i + 1, at[i], bt[i], pr[i], ct[i], tat[i], wt[i]);
+        printf("%d\t%d\t%d\t%d\t%d\t%d\n", i + 1, at[i], bt[i], ct[i], tat[i], wt[i]);
     }
 
     printf("Average TAT: %.2f\n", total_tat / n);
@@ -94,29 +65,19 @@ void priorityNonPreemptive(int n, int at[], int bt[], int pr[]) {
 }
 
 int main() {
-    int n, choice;
+    int n, quant;
     printf("Enter number of processes: ");
     scanf("%d", &n);
 
-    int at[n], bt[n], pr[n];
+    int at[n], bt[n];
     for (int i = 0; i < n; i++) {
-        printf("Enter AT, BT, and Priority P%d: ", i + 1);
-        scanf("%d %d %d", &at[i], &bt[i], &pr[i]);
+        printf("Enter AT and BT for process %d: ", i + 1);
+        scanf("%d %d", &at[i], &bt[i]);
     }
 
-    printf("\nChoose Scheduling Algorithm:\n");
-    printf("1. Preemptive Priority Scheduling\n");
-    printf("2. Non-Preemptive Priority Scheduling\n");
-    printf("Enter choice: ");
-    scanf("%d", &choice);
+    printf("Enter time quantum: ");
+    scanf("%d", &quant);
 
-    if (choice == 1) {
-        priorityPreemptive(n, at, bt, pr);
-    } else if (choice == 2) {
-        priorityNonPreemptive(n, at, bt, pr);
-    } else {
-        printf("Invalid choice!\n");
-    }
-
+    roundRobin(n, at, bt, quant);
     return 0;
 }
